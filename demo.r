cdb8345d388f9ec9985e22aa1857380a426a0ede
Arie Bates-Hermans <batesarie@ecs.vuw.ac.nz>
2021-09-29 14:57:58 +1300

    added z

--- demo.r
+++ demo.r
@@ -1,5 +1,6 @@
 library(tidyverse)
 x <- rnorm(100)
 y <- rnorm(100)
+z <- rnorm(100)
 df <- as.tibble(x, y)
-df %>% ggplot(aes(x= x, y = y)) + geom_point()
+df %>% ggplot(aes(x= x, y = y)) + geom_point(size=z)
