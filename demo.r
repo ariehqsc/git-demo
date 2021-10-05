bbbe7a8d2fb29b0ebb5a438d6f07754d5089b9ab
Arie Bates-Hermans <batesarie@ecs.vuw.ac.nz>
2021-10-06 11:53:56 +1300

    changed back

--- demo.r
+++ demo.r
@@ -1,7 +1,12 @@
 library(tidyverse)
 x <- rnorm(100)
 y <- rnorm(100)
+<<<<<<< HEAD
 z <- rnorm(100)
 df <- tibble(x, y)
 df %>% ggplot(aes(x= x, y = y)) + geom_point(size=z)
 ggsave("output.png")
+=======
+df <- as.tibble(x, y)
+df %>% ggplot(aes(x= x, y = y)) + geom_point()
+>>>>>>> parent of cdb8345 (added z)
